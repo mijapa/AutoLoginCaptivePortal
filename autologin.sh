7fc5ea862410dfa1983fddc58591ee992901f7b0
Michał <michaljanpatyk@gmail.com>
2020-08-18 08:04:40 +0200

    Add cosmetic changes

--- autologin.sh
+++ autologin.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+#simple script checking connection and autologin to captive portal
 
 #extract post url with browser developer tool and put it in url file
 #extract query with browser developer tool and put it in query file
 
 #make autologin.sh executable
 #chmod +x autologin.sh
-#change path and add to cronetab -e
+
+#change path and add to cronetab -e for execution every one second
 # * * * * * for i in {1..60}; do /path/autologin.sh & sleep 1; done
+
 #restart cron
 #sudo service cron reload
 
-
 DBG=true
-
-#reading from file
-url=$(<url)
 
 [[ $DBG ]] && echo "Trying to access google"
 
@@ -24,11 +23,13 @@ status=$(curl google.com/generate_204 -I -s | head -n 1 | cut '-d ' '-f2')
 
 if [ "${status}" -eq "204" ]
 then
- [[ $DBG ]] && echo OK
+  [[ $DBG ]] && echo OK
 else
- [[ $DBG ]] && echo "Captive Portal - sending POST"
+  [[ $DBG ]] && echo "Reading url from file"
+  url=$(<url)
 
- curl \
- -d @query \
- "${url}" -v
+  [[ $DBG ]] && echo "Captive Portal - sending POST"
+  curl \
+  -d @query \
+  "${url}" -v
 fi
