c6c0791143068ef7a4077008a8a9c875d42feb64
Michał <michaljanpatyk@gmail.com>
2020-08-18 13:10:34 +0200

    Create function

--- autologin.sh
+++ autologin.sh
@@ -7,29 +7,33 @@
 #make autologin.sh executable
 #chmod +x autologin.sh
 
-#change path and add to cronetab -e for execution every one second
-# * * * * * for i in {1..60}; do /path/autologin.sh & sleep 1; done
+#change path and add to cronetab -e
+# * * * * * /path/autologin.sh
 
 #restart cron
 #sudo service cron reload
 
-DBG=true
+function checkAndLogin {
+  DBG=true
 
-[[ $DBG ]] && echo "Trying to access google"
+  [[ $DBG ]] && echo "Trying to access google"
 
-status=$(curl google.com/generate_204 -I -s | head -n 1 | cut '-d ' '-f2')
+  status=$(curl google.com/generate_204 -I -s | head -n 1 | cut '-d ' '-f2')
 
-[[ $DBG ]] && echo "Return status: ""${status}"
+  [[ $DBG ]] && echo "Return status: ""${status}"
 
-if [ "${status}" -eq "204" ]
-then
-  [[ $DBG ]] && echo OK
-else
-  [[ $DBG ]] && echo "Reading url from file"
-  url=$(<url)
+  if [ "${status}" -eq "204" ]
+  then
+    [[ $DBG ]] && echo OK
+  else
+    [[ $DBG ]] && echo "Reading url from file"
+    url=$(<url)
 
-  [[ $DBG ]] && echo "Captive Portal - sending POST"
-  curl \
-  -d @query \
-  "${url}" -v
-fi
+    [[ $DBG ]] && echo "Captive Portal - sending POST"
+    curl \
+    -d @query \
+    "${url}" -v
+  fi
+}
+
+for i in {1..60}; do checkAndLogin & sleep 1; done
